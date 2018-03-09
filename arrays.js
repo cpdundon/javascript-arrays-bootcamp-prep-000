aa13435def9ce9638da03dc0ab1401c4088ca551
Chris Dundon <chris.dundon@gmail.com>
2018-03-09 21:19:54 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,44 @@
+var chocolateBars = [`snickers`, `hundred grand`, `kitkat`, `skittles`];
+
+function addElementToBeginningOfArray(arr, elem) {
+  return [elem, ...arr];
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, elem) {
+  arr.unshift(elem)
+  return arr;
+}
+
+function addElementToEndOfArray(arr, elem) {
+  return [...arr, elem];
+}
+
+function destructivelyAddElementToEndOfArray(arr, elem) {
+  arr.push(elem)
+  return arr;
+}
+
+function accessElementInArray(arr, idx) {
+  return arr[idx];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr) {
+  arr.shift();
+  return arr;
+}
+
+function removeElementFromBeginningOfArray(arr){
+  temp = arr.slice(1);
+  return temp;
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr){
+  arr.pop();
+  return arr;
+}
+
+function removeElementFromEndOfArray(arr){
+  temp = arr.slice(0, arr.length - 1);
+  return temp;
+}
+
